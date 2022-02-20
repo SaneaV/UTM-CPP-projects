bcf21f27fa471edba6a8babea7406eb38463fb96
Alexandr Vicev <saneavicev@mail.ru>
2022-02-20 12:20:11 +0200

    Adjust with AC

--- 1.2/lab10-11/funtionality.h
+++ 1.2/lab10-11/funtionality.h
@@ -2,24 +2,52 @@
 #include <iomanip>
 #include <fstream>
 
+struct Country {
+    char name[50];
+    char capital[50];
+    int area;
+    int population;
+    int grossDomesticProduct;
+};
+
 class Node {
 
     Node() {}
 
 public:
-    Node(int data) : data(data) {}
+    explicit Node(Country *country) : country(*country) {}
 
-    int data;
+    Country country{};
     Node *left = nullptr;
     Node *right = nullptr;
 };
 
-void insertRecursive(Node *&root, int key);
+void insertRecursive(Node *&root, Country *country);
 
-void insertIterative(Node *&root, int key);
+void insertIterative(Node *&root, Country *country);
 
-int inputNumberFor(char *message);
+void showRecursiveLeftNodeRight(Node *root);
 
-void searchRecursive(Node *root, int key, Node *parent);
+void showIterativeNodeRight(Node *root);
 
-void searchIterative(Node *root, int key);
+void showRecursiveRightNodeLeft(Node *root);
+
+void showIterativeRightNodeLeft(Node *root);
+
+void searchRecursive(Node *root, std::string country, Node *parent);
+
+void searchIterative(Node *root, std::string country);
+
+int countNumberOfNodesIterative(Node *root);
+
+int countNumberOfNodesRecursive(Node *root);
+
+int calculateHeightIterative(Node *root);
+
+int calculateHeightRecursive(Node *root);
+
+void deleteTreeRecursive(Node *root);
+
+void deleteTreeIterative(Node *root);
+
+Country *inputCountryData();
